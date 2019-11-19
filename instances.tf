2776a1b7b4e513d95eb76d3f151026f85de3c00b
Jwanderema <wanderema@hotmail.co.uk>
2019-11-19 10:41:44 +0000

    instance profile name change

--- instances.tf
+++ instances.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "tableau" {
   vpc_security_group_ids      = ["${aws_security_group.tableau.id}"]
   subnet_id                   = "${aws_subnet.tableau_subnet.id}"
   private_ip                  = "${var.tableau_dev_ip}"
-  iam_instance_profile        = "${aws_iam_role.tableau.name}"
+  iam_instance_profile        = "${aws_iam_instance_profile.tableau.id}"
   associate_public_ip_address = false
   monitoring                  = true
 
