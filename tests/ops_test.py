eb642595569c060f40fe3c0238f9c7c2fb941030
Jwanderema <wanderema@hotmail.co.uk>
2020-01-23 09:53:51 +0000

    reverting name change

--- tests/ops_test.py
+++ tests/ops_test.py
@@ -25,7 +25,7 @@ class TestE2E(unittest.TestCase):
               az                        = "eu-west-2a"
               naming_suffix             = "ops-preprod-dq"
               tableau_dev_ip            = "1.2.3.4"
-              tableau2_ip     = "1.2.3.5"
+              tableau_deployment_ip     = "1.2.3.5"
               opsvpc_id                 = "1234"
               tableau_subnet_cidr_block = "10.2.1.0/24"
               vpc_subnet_cidr_block     = "10.2.1.0/24"
