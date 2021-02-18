3e30a80586dc03bbde0ede8339709329f637060c
JWanderema <wanderema@hotmail.co.uk>
2021-02-18 13:43:16 +0000

    remove fedat

--- tests/ops_test.py
+++ tests/ops_test.py
@@ -33,7 +33,6 @@ class TestE2E(unittest.TestCase):
               apps_aws_bucket_key       = "1234"
               namespace                 = "NOTPROD"
               dq_pipeline_ops_readwrite_database_name_list       = ["api_input"]
-              dq_pipeline_ops_readwrite_database_name_list_fedat = ["fedat_reporting"]
               dq_pipeline_ops_readonly_database_name_list        = ["api_input"]
               dq_pipeline_ops_readwrite_bucket_list              = ["s3-bucket-name"]
               dq_pipeline_ops_readonly_bucket_list               = ["s3-bucket-name"]
