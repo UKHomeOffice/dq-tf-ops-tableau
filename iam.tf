7aae4f2178e18b9e274efffadda6d4c999288e12
JWanderema <wanderema@hotmail.co.uk>
2021-12-07 19:16:51 +0000

    attach managed policy

--- iam.tf
+++ iam.tf
@@ -1,4 +1,5 @@
 resource "aws_iam_role" "tableau" {
+  name               = "ops-win-tableau"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -170,6 +171,11 @@ EOF
 resource "aws_iam_role_policy_attachment" "ops_win_athena" {
   role       = aws_iam_role.tableau.name
   policy_arn = aws_iam_policy.ops_win_athena.arn
+}
+
+resource "aws_iam_role_policy_attachment" "dq_tf_infra_write_to_cw" {
+  role       = aws_iam_role.tableau.id
+  policy_arn = "arn:aws:iam::${var.account_id[var.namespace]}:policy/dq-tf-infra-write-to-cw"
 }
 
 resource "aws_iam_instance_profile" "tableau" {
