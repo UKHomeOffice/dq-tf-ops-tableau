6645640f6072158bc2ce54b1860c34a48defc06f
JWanderema <wanderema@hotmail.co.uk>
2021-12-07 19:16:51 +0000

    attach managed policy

--- variable.tf
+++ variable.tf
@@ -1,3 +1,12 @@
+variable "account_id" {
+  type = map(string)
+  default = {
+    "test"    = "797728447925"
+    "notprod" = "483846886818"
+    "prod"    = "337779336338"
+  }
+}
+
 variable "ops_config_bucket" {
 }
 
